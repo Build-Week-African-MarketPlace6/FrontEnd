2bd58af10a5c06db485a5a8e8e2c5d9f6f59bd5a
Anthony <aamay022@fiu.edu>
2021-11-15 18:17:43 -0500

    product-list

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,12 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import AddForm from './components/AddForm';
+import { useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ShoppingCart from './components/ShoppingCart';
+
+
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
+  const addItem = item => {
+		setCart([...cart, item])
+	};
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter((i) => i.item_id !== id));
+  }
+
   return (
+    <Router>
     <div className="App">
-      <AddForm/>
+
+      <Link to='/productList'>Products</Link>
+      <Link to='/cart'>Cart</Link>
+
+   <Switch>
+      <Route exact path="/">
+        <div><h1>Home Page</h1></div>
+      </Route>
+      
+      <Route path = '/productList'>
+        <ProductList addItem={addItem}/>
+      </Route>
+
+      <Route path="/cart">
+				<ShoppingCart cart ={cart} removeFromCart={removeFromCart}/>
+			</Route>
+
+   </Switch>
     </div>
+    </Router>
   );
 }
 
--- african-marketplace/src/components/AddForm.js
+++ african-marketplace/src/components/AddForm.js
@@ -1,31 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+const AddForm = (props) => {
 
-class AddForm extends React.Component {
-    state = {
-        itemData: {
-            item: "",
-            price: "", 
-            description: "",
-            id: ""
-        }
-    };
+    const [newItem, setNewItem] = useState({
+        item_name:"",
+        item_price:"",
+        item_id: Date.now(),
+        item_description:"",
+    });
 
-    handleChange = event => {
-        this.setState({
-            itemData: {
-                ...this.state.itemData,
-                [event.target.name]: event.target.value
-            }
-        })
+    const [error, setError] = useState('')
+
+    const handleChange = e => {
+        setNewItem({
+            ...newItem,
+            [e.target.name]:e.target.value
+        });
     }
 
-    render() {
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (newItem.item_name === "" || newItem.item_price === "" || newItem.item_description === "") {
+            setError('Must fill out all')
+    }else{
+        props.setProducts([...props.products, newItem])
+    //     axios.post(`https://marketplacelambda.herokuapp.com/api/shelf`, newItem)
+    //     .then(res => {
+    //         props.setProducts(res.data);
+    //     })
+    //     .catch(err=> {
+    //         console.log(err);
+    //       })
+    }
+        
+    }
         return(
             <div>
-
+                <h1>Add Product</h1>
+                <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="item_name">Name:</label><br/>
+                <input onChange={handleChange} value={newItem.item_name} name="item_name" id="item_name" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="item_price">Price:</label><br/>
+                <input onChange={handleChange} value={newItem.item_price} name="item_price" id="item_price" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="item_description">Description:</label><br/>
+                <input onChange={handleChange} value={newItem.item_description} name="item_description" id="item_description" />
+            </div>
+            {
+              error && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {error}</div>
+            }
+            <button>Submit Product</button>
+        </form>
             </div>)
-    }
+
 }
 
 export default AddForm;
