d5e3cad6aa006ef358bd4392f57123f0e7753f8d
Angelique Abacajan <ma_utsey@yahoo.com>
2021-11-15 17:42:40 -0500

    Pulse Check: Verifying if code is rendering

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import AddForm from './components/AddForm';
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome to the African Marketplace</h1>
+      <p>Sauti Africa empowers small business owners, particularly women, to improve their business and economic opportunities to grow out of poverty. </p>
       <AddForm/>
     </div>
   );
