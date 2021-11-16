b1d5c76728a3d3814ca5c35442c8d11dac18214e
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-16 15:03:22 -0600

    improvements to git featured section

--- african-marketplace/src/components/HomeContent/Featured.js
+++ african-marketplace/src/components/HomeContent/Featured.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../assets/css/Home.css';
+import '../../assets/css/Featured.css';
 import featuredOne from '../../assets/images/alexander-schimmeck-pKSm8hV62eI-unsplash.jpg';
 import featuredTwo from '../../assets/images/karim-manjra-fm9wqDNXyQ0-unsplash.jpg';
 import featuredThree from '../../assets/images/kristina-balic-M13V8hgvm-E-unsplash.jpg';
@@ -11,54 +11,47 @@ class Featured extends React.Component {
 				<div className='container'>
 					<header>
 						<h2>
-							Monthly Featured <strong>Artist</strong>
+							Featured <strong>Artists</strong>
 						</h2>
 					</header>
 					<div className='row aln-center'>
 						<div className='col-4 col-6-medium col-12-small'>
 							<section>
-								<a href='#' className='image featured'>
-									<img src={featuredOne} alt='' />
-								</a>
-								<header>
-									<h3>Featured First Item</h3>
-								</header>
-								<p>
-									This is <strong>Strongly Typed</strong>, a free,
-									fully responsive site template by HTML5 UP. Free for
-									personal and commercial use under the CCA 3.0
-									license.
-								</p>
+								<figure class='snip1401'>
+									<img src={featuredOne} alt='featured image 1' />
+									<figcaption>
+										<h3>Eleanor Fant</h3>
+										<p>Unique works in textile design.</p>
+									</figcaption>
+									<i class='ion-ios-basket-outline'></i>
+									<a href='../ProductList'></a>
+								</figure>
 							</section>
 						</div>
 						<div className='col-4 col-6-medium col-12-small'>
 							<section>
-								<a href='#' className='image featured'>
-									<img src={featuredTwo} alt='' />
-								</a>
-								<header>
-									<h3>Nice! What is HTML5 UP?</h3>
-								</header>
-								<p>
-									HTML5 UP is a side project of AJ’s(= me). I started
-									it as a way to both test my responsive tools and
-									sharpen up my coding and design skills a bit.
-								</p>
+								<figure class='snip1401'>
+									<img src={featuredTwo} alt='featured image 1' />
+									<figcaption>
+										<h3>Anyce Devolent</h3>
+										<p>Home goods with African flair.</p>
+									</figcaption>
+									<i class='ion-ios-basket-outline'></i>
+									<a href='../ProductList'></a>
+								</figure>
 							</section>
 						</div>
 						<div className='col-4 col-6-medium col-12-small'>
 							<section>
-								<a href='#' className='image featured'>
-									<img src={featuredThree} alt='' />
-								</a>
-								<header>
-									<h3>What's this built with?</h3>
-								</header>
-								<p>
-									<strong>Responsive Tools</strong> is a simple set of
-									tools for building responsive sites and apps. All of
-									my templates at HTML5 UP are built using these tools.
-								</p>
+								<figure class='snip1401'>
+									<img src={featuredThree} alt='featured image 1' />
+									<figcaption>
+										<h3>Selice Gordon</h3>
+										<p>Texture and style bring soaps to life.</p>
+									</figcaption>
+									<i class='ion-ios-basket-outline'></i>
+									<a href='../ProductList'></a>
+								</figure>
 							</section>
 						</div>
 					</div>
