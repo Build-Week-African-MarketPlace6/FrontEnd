67924e4ceff059a083c4353a8d96bb9a65829eb0
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-16 09:23:24 -0600

    imporoved banner section

--- african-marketplace/src/components/HomeContent/Banner.js
+++ african-marketplace/src/components/HomeContent/Banner.js
@@ -4,7 +4,7 @@ class Banner extends React.Component {
 	render() {
 		return (
 			<div>
-				<section className='banner'>
+				<section id='banner'>
 					<div className='container'>
 						<p>
 							"The most effective way to do it, is to do it." --Amelia
