b6b0f33dd788c7142b36ae1cb01f044a78516da7
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-15 19:42:47 -0600

    added images for home page and fixed title

--- african-marketplace/src/components/Home.js
+++ african-marketplace/src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Featured from './HomeContent/Featured';
 import Banner from './HomeContent/Banner';
 import Content from './HomeContent/Content';
+// import headImage from '../assets/images/alexander-schimmeck-pKSm8hV62eI-unsplash.jpg';
+import '../assets/css/Home.css';
 
 class Home extends React.Component {
 	render() {
